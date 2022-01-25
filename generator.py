f19fc6f835deaec7695820846e601a784fc327b7
bruseO4 <71778268+bruseO4@users.noreply.github.com>
2022-01-25 14:57:35 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,7 @@
-print ("Hello")
+import os
+import openai
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(engine="text-ada-001", prompt="Say this is a test", max_tokens=6)
